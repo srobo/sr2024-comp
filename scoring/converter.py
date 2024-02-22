710566d23be4f7fe82ba9ce450376e222e5f9de0
Peter Law <PeterJCLaw@gmail.com>
2024-02-22 17:51:09 +0000

    Add a copy of the base converter from srcomp-scorer
    This is a baseline we can modify for this year, rather than trying
    to extend it by overriding.

--- scoring/converter.py
+++ scoring/converter.py
@@ -1,12 +1,79 @@
-from sr.comp.scorer import Converter as BaseConverter
-
-
-class Converter(BaseConverter):
+class Converter:
     def form_team_to_score(self, form, zone_id):
-        raise NotImplementedError
+        """
+        Prepare the part of the score dict for the given zone from the form data.
+        """
+        return {
+            'zone': zone_id,
+            'disqualified':
+                form.get(f'disqualified_{zone_id}', None) is not None,
+            'present':
+                form.get(f'present_{zone_id}', None) is not None,
+        }
+
+    def form_to_score(self, match, form):
+        """
+        Prepare a score dict for the given match and form dict.
+
+        This method is used to convert the submitted information for storage as
+        YAML in the compstate.
+        """
+        zone_ids = range(len(match.teams))
+
+        teams = {}
+        for zone_id in zone_ids:
+            tla = form.get(f'tla_{zone_id}', None)
+            if tla:
+                teams[tla] = self.form_team_to_score(form, zone_id)
+
+        zones = list(zone_ids) + ['other']
+        arena = {}
+        for zone in zones:
+            arena[zone] = {'tokens': form.get(f'tokens_{zone}', '')}
+
+        return {
+            'arena_id': match.arena,
+            'match_number': match.num,
+            'teams': teams,
+            'arena_zones': arena,
+        }
 
     def score_to_form(self, score):
-        raise NotImplementedError
+        """
+        Prepare a form dict for the given score dict.
 
-    def match_to_form(self, match):
-        raise NotImplementedError
+        This method is used when there is an existing score for a match.
+        """
+        form = {}
+
+        for tla, info in score['teams'].items():
+            zone_id = info['zone']
+            form[f'tla_{zone_id}'] = tla
+            form[f'disqualified_{zone_id}'] = info.get('disqualified', False)
+            form[f'present_{zone_id}'] = info.get('present', True)
+
+        for zone, info in score.get('arena_zones', {}).items():
+            form[f'tokens_{zone}'] = info['tokens'].upper()
+
+        return form
+
+    def match_to_form(self, match: Match) -> dict[str, str | bool]:
+        """
+        Prepare a fresh form dict for the given match.
+
+        This method is used when there is no existing score for a match.
+        """
+
+        form: dict[str, str | bool] = {}
+
+        for zone_id, tla in enumerate(match.teams):
+            if tla:
+                form[f'tla_{zone_id}'] = tla
+                form[f'disqualified_{zone_id}'] = False
+                form[f'present_{zone_id}'] = False
+
+            form[f'tokens_{zone_id}'] = ''
+
+        form['tokens'] = ''
+
+        return form
